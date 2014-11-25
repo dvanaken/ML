628c1ba1ddc60c03ded6e9211d5b5189ab85b81e
Dana Marie Van Aken <dvanaken@gs14445.sp.cs.cmu.edu>
2014-11-24 19:49:34 -0500

    ml

--- HW4/hw4/code/conditional_given_all.m
+++ HW4/hw4/code/conditional_given_all.m
@@ -20,5 +20,22 @@ function [probability] = conditional_given_all(model, var_id)
 probability = ones(1, length(model{var_id}.values));
 probability = probability / sum(probability);
 
+var = model{var_id};
+var
+model{1}
+var_parents = conditional_given_parents(model,var_id);
+
+product = 1;
+for i=1:length(var.children)
+    child_id = var.children(i);
+    child = model{child_id};
+    child
+    c_parents = conditional_given_parents(model, child_id);
+    c_prob = c_parents(child.value + 1);
+    product = product * c_prob;
+end
+
+probability = var_parents .* product;
+
 end
 
