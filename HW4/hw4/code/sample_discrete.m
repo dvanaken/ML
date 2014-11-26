96d2b2ae07d5fa0af1cb48544802595b0d73e7a8
dana <danava04@XPS.(none)>
2014-11-26 11:29:30 -0500

    hw4

--- HW4/hw4/code/sample_discrete.m
+++ HW4/hw4/code/sample_discrete.m
@@ -3,11 +3,8 @@ function [sample] = sample_discrete(model, var_id)
 % of the variable var_id, given that the value of every
 % other variable j in the model is 'model{j}.value'.
 
-% TODO: implement me!
 var = model{var_id};
-values = var.values;
 [probability] = conditional_given_all(model,var_id);
-%sample = randi([1, length(values)]) - 1;
 sample = discrete_rnd(model{var_id}.values, probability,1);
 end
 
