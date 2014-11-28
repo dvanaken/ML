9972692375beafa8c0a7a838bc2a2005abc1cd7b
dana <danava04@XPS.(none)>
2014-11-28 18:05:34 -0500

    hw4 completed

--- HW4/hw4/code/mcmc_test_convergence.m
+++ HW4/hw4/code/mcmc_test_convergence.m
@@ -21,9 +21,9 @@ function [model] = mcmc(model, observed_variables, observed_values, iterations)
     model, observed_variables, observed_values, iterations);
 
 bound = 0.01;
-actual_probs = [.947, .031, .022];
-actual_percentages = actual_probs * bound;
-probs = [0,0,0];
+actual_probs = [.5243, .4757];
+%actual_percentages = actual_probs * bound;
+probs = [0,0];
 
 for i = 1:iterations
 	perm = randperm(length(unknown_variables));
@@ -35,18 +35,18 @@ for i = 1:iterations
         model{id}.value = newval;
     end
     
-    probs(1) = sum(model{1}.samples(1:i) == 0)/i;
-    probs(2) = sum(model{1}.samples(1:i) == 1)/i;
-    probs(3) = sum(model{1}.samples(1:i) == 2)/i;
+    probs(1) = sum(model{3}.samples(1:i) == 0)/i;
+    probs(2) = sum(model{3}.samples(1:i) == 1)/i;
+    %probs(3) = sum(model{1}.samples(1:i) == 2)/i;
     
     error(1) = abs((probs(1) - actual_probs(1)) / actual_probs(1));
     error(2) = abs((probs(2) - actual_probs(2)) / actual_probs(2));
-    error(3) = abs((probs(3) - actual_probs(3)) / actual_probs(3));
+    %error(3) = abs((probs(3) - actual_probs(3)) / actual_probs(3));
     
     sum_below = sum(error < bound);
     
     %if abs(probs - actual_probs)/ < actual_percentages
-    if sum_below == 3
+    if sum_below == length(probs)
          %test_runs(j) = i;
          %avg = avg + i;
          error
@@ -57,7 +57,7 @@ for i = 1:iterations
     if mod(i, 100) == 0
         i 
         error
-         probs
+        probs
     end
     
 end
--- HW4/hw4/code/script.m
+++ HW4/hw4/code/script.m
@@ -1,5 +1,5 @@
 model = load_diagnosis();
-model = mcmc_test_convergence(model, 5, 1, 100000);
+model = mcmc_test_convergence(model, [4, 5, 1], [1, 1, 2], 1000000);
 
 
     
